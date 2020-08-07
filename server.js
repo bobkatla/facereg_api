2443d41817f8828a17615a8fa6fc5dc0c096db24
bobkatla <bertram.99.works@gmail.com>
2020-08-07 10:39:48 +1000

    add cors

--- server.js
+++ server.js
@@ -25,6 +25,12 @@ const express = require('express');
 const app = express();
 
 // middleware
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-RequestedWith, Content-Type, Accept");
+    next();
+});  
+
 app.use(express.urlencoded({extended: false}))
 app.use(express.json());
 app.use(cors());
