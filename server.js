3191582c893abacbc5ccee69579412482025a8f0
bobkatla <bertram.99.works@gmail.com>
2020-08-07 12:38:51 +1000

    test cors

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ const app = express();
 //     next();
 // });
 const corsOptions = {
-    origin: 'localhost:3000',
+    origin: 'http://localhost:3000',
     optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
   }
 
