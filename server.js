c4c7b3196495239a1adf735487afa3c1bfcc170a
bobkatla <bertram.99.works@gmail.com>
2020-08-07 10:53:28 +1000

    fix cors

--- server.js
+++ server.js
@@ -27,9 +27,9 @@ const app = express();
 // middleware
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-RequestedWith, Content-Type, Accept");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
-});  
+});
 
 app.use(express.urlencoded({extended: false}))
 app.use(express.json());
